f6da956cdc290abfd518bcec57e66876fc8af295
Desenvolvimento <repository.desenvolvimento@gmail.com>
2021-05-11 00:42:32 -0300

    SISGERREDSOCAPP-20210510195106

--- src/app/components/tweet/tweet.component.ts
+++ src/app/components/tweet/tweet.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tweet',
@@ -7,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TweetComponent implements OnInit {
 
+  @Input("tweetObject") tweetObject: any;
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  parsetweet() {
+    this.tweetObject.text = this.tweetObject.text.replace(/\#[a-zA-Z]+/g, "<span class\=\"hightlight\"\>$&<\/span\>");
+    this.tweetObject.text = this.tweetObject.text.replace(/\@[a-zA-Z]+/g, "<span class\=\"hightlight\"\>$&<\/span\>");
+  }
 
 }
